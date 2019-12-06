b9d51a0036931a062d128dd8556209418b61754d
Finlay <finlay@blackfordanalysis.com>
2019-12-06 11:29:12 +0000

    read six input

--- six/src/main.rs
+++ six/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("six/input")?;
+    let reader = BufReader::new(f);
+    let mut masses = HashMap::new();
+    for l in reader.lines(){
+        let l = l.unwrap();
+        let parts : Vec<_> = l.split(")").collect();
+        let major = parts[0].to_owned();
+        let minor = parts[1].to_owned();
+        let orbitors = masses.entry(major).or_insert(Vec::new());
+        (*orbitors).push(minor);
+    }
+    for (k,v) in masses {
+        println!("{} {}", k, v.len());
+    }
+    Ok(())
+}
+
+fn count_orbits(masses : HashMap<String, Vec<String>>) -> i32 {
+    return 0;
 }
