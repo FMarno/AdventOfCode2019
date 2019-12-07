809b74effc5ced46e2caa02b1bf505a8ee8a27a2
FMarno <finlay.marno@gmail.com>
2019-12-07 14:27:14 +0000

    seven part 1

--- seven/src/main.rs
+++ seven/src/main.rs
@@ -1,13 +1,37 @@
 use std::fs;
+use std::collections::VecDeque;
+use permutohedron::heap_recursive;
 
 fn main() {
-    let input = fs::read_to_string("five/input").expect("can't open file");
+    let mut inputs : Vec<_> = (0..5).collect();
+    let mut permutations = Vec::new();
+    heap_recursive(&mut inputs,|x| permutations.push(x.to_owned()));
+    let input = fs::read_to_string("seven/input").expect("can't open file");
     let codes : Vec<i32> = input.trim().split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-    let mut output = Vec::new();
-    run_codes(codes,5, &mut output);
-    for x in output {
-        print!("{} " ,x);
-    }
+    let m = permutations.iter()
+        .map(
+            |x| thruster_signal(x.to_vec(), codes.to_vec())
+        )
+        .max();
+    println!("{:?}",m.expect("fuck"));
+}
+
+fn thruster_signal(input : Vec<i32>, codes : Vec<i32>) -> i32 {
+    let amps = input.iter().map(|x|{
+        let mut inputs : VecDeque<i32> = VecDeque::new();
+        inputs.push_back(*x);
+        Amp{codes:codes.to_vec(), pointer: 0, input:inputs}
+    });
+    amps.fold(0, |output, mut amp| {
+        amp.input.push_back(output);
+        amp.run_codes().expect("NO OUTPUT")
+    })
+}
+
+struct Amp {
+    codes : Vec<i32>,
+    pointer : usize,
+    input : VecDeque<i32>,
 }
 
 #[derive(Debug)]
@@ -24,124 +48,127 @@ enum OpCode {
     Error(i32),
 }
 
-fn run_codes(mut codes :Vec<i32>, input : i32, output : &mut Vec<i32>) -> i32 {
-    use OpCode::*;
+impl Amp {
+    fn run_codes(&mut self) -> Option<i32> {
+        use OpCode::*;
+        let codes = &mut self.codes;
+        let input = &mut self.input;
+        let pointer = &mut self.pointer;
 
-    let mut pointer = 0;
-    loop{
-        println!("{} {:?}",pointer, codes);
-        let code = codes[pointer];
-        let (_mode3, mode2, mode1, instruction) = decode_op(code);
-        println!("{} {} {} {:?}", _mode3, mode2, mode1, instruction);
-        match instruction {
-            Add => {
-                // sum ptr+1 and ptr+2 and save to ptr+3
-                let mut l = codes[pointer+1];
-                if !mode1 {
-                    l = codes[l as usize];
-                }
-                let mut r = codes[pointer+2];
-                if !mode2 {
-                    r = codes[r as usize];
-                }
-                let o = codes[pointer+3];
-                codes[o as usize] = l + r;
-                pointer += 4;
-            },
-            Mult => {
-                // multiply ptr+1 and ptr+2 and save to ptr+3
-                let mut l = codes[pointer+1];
-                if !mode1 {
-                    l = codes[l as usize];
-                }
-                let mut r = codes[pointer+2];
-                if !mode2 {
-                    r = codes[r as usize];
-                }
-                let o = codes[pointer+3];
-                codes[o as usize] = l*r;
-                pointer += 4;
-            },
-            Load => {
-                // save input to pointer+1
-                let location = codes[pointer+1];
-                codes[location as usize] = input;
-                pointer += 2;
-            },
-            Save => {
-                // save pointer+1 to output
-                let mut location = codes[pointer+1];
-                if !mode1 {
-                    location = codes[location as usize];
-                }
-                output.push(location);
-                pointer += 2;
-            },
-            JumpTrue => {
-                let mut test = codes[pointer+1];
-                if !mode1 {
-                    test = codes[test as usize];
-                }
-                if test != 0 {
-                    let mut location = codes[pointer+2];
+        loop{
+            //println!("{} {:?}",pointer, codes);
+            let code = codes[*pointer];
+            let (_mode3, mode2, mode1, instruction) = decode_op(code);
+            //println!("{} {} {} {:?}", _mode3, mode2, mode1, instruction);
+            match instruction {
+                Add => {
+                    // sum ptr+1 and ptr+2 and save to ptr+3
+                    let mut l = codes[*pointer+1];
+                    if !mode1 {
+                        l = codes[l as usize];
+                    }
+                    let mut r = codes[*pointer+2];
                     if !mode2 {
+                        r = codes[r as usize];
+                    }
+                    let o = codes[*pointer+3];
+                    codes[o as usize] = l + r;
+                    *pointer += 4;
+                },
+                Mult => {
+                    // multiply ptr+1 and ptr+2 and save to ptr+3
+                    let mut l = codes[*pointer+1];
+                    if !mode1 {
+                        l = codes[l as usize];
+                    }
+                    let mut r = codes[*pointer+2];
+                    if !mode2 {
+                        r = codes[r as usize];
+                    }
+                    let o = codes[*pointer+3];
+                    codes[o as usize] = l*r;
+                    *pointer += 4;
+                },
+                Load => {
+                    // save input to *pointer+1
+                    let location = codes[*pointer+1];
+                    codes[location as usize] = input.pop_front().unwrap();
+                    *pointer += 2;
+                },
+                Save => {
+                    // save *pointer+1 to output
+                    let mut location = codes[*pointer+1];
+                    if !mode1 {
                         location = codes[location as usize];
                     }
-                    pointer = location as usize;
-                } else {
-                    pointer += 3;
-                }
-            },
-            JumpFalse => {
-                let mut test = codes[pointer+1];
-                if !mode1 {
-                    test = codes[test as usize];
-                }
-                if test == 0 {
-                    let mut location = codes[pointer+2];
-                    if !mode2 {
-                        location = codes[location as usize];
+                    *pointer += 2;
+                    return Some(location);
+                },
+                JumpTrue => {
+                    let mut test = codes[*pointer+1];
+                    if !mode1 {
+                        test = codes[test as usize];
                     }
-                    pointer = location as usize;
-                } else {
-                    pointer += 3;
+                    if test != 0 {
+                        let mut location = codes[*pointer+2];
+                        if !mode2 {
+                            location = codes[location as usize];
+                        }
+                        *pointer = location as usize;
+                    } else {
+                        *pointer += 3;
+                    }
+                },
+                JumpFalse => {
+                    let mut test = codes[*pointer+1];
+                    if !mode1 {
+                        test = codes[test as usize];
+                    }
+                    if test == 0 {
+                        let mut location = codes[*pointer+2];
+                        if !mode2 {
+                            location = codes[location as usize];
+                        }
+                        *pointer = location as usize;
+                    } else {
+                        *pointer += 3;
+                    }
+                },
+                LessThan => {
+                    let mut l = codes[*pointer+1];
+                    if !mode1 {
+                        l = codes[l as usize];
+                    }
+                    let mut r = codes[*pointer+2];
+                    if !mode2 {
+                        r = codes[r as usize];
+                    }
+                    let o = codes[*pointer+3];
+                    codes[o as usize] = if l < r { 1} else {0};
+                    *pointer += 4;
+                },
+                Equals => {
+                    let mut l = codes[*pointer+1];
+                    if !mode1 {
+                        l = codes[l as usize];
+                    }
+                    let mut r = codes[*pointer+2];
+                    if !mode2 {
+                        r = codes[r as usize];
+                    }
+                    let o = codes[*pointer+3];
+                    codes[o as usize] = if l == r { 1} else {0};
+                    *pointer += 4;
+                },
+                Halt => return None,
+                Error(e) => {
+                    println!("Unexpected code! {}" ,e);
+                    return None;
                 }
-            },
-            LessThan => {
-                let mut l = codes[pointer+1];
-                if !mode1 {
-                    l = codes[l as usize];
-                }
-                let mut r = codes[pointer+2];
-                if !mode2 {
-                    r = codes[r as usize];
-                }
-                let o = codes[pointer+3];
-                codes[o as usize] = if l < r { 1} else {0};
-                pointer += 4;
-            },
-            Equals => {
-                let mut l = codes[pointer+1];
-                if !mode1 {
-                    l = codes[l as usize];
-                }
-                let mut r = codes[pointer+2];
-                if !mode2 {
-                    r = codes[r as usize];
-                }
-                let o = codes[pointer+3];
-                codes[o as usize] = if l == r { 1} else {0};
-                pointer += 4;
-            },
-            Halt => return codes[0],
-            Error(e) => {
-                println!("Unexpected code! {}" ,e);
-                return 0;
             }
         }
-    }
-}        
-
+    }        
+}
 fn decode_op(op : i32) -> (bool,bool,bool,OpCode) {
     use OpCode::*;
 
