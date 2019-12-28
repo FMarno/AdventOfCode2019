2ec48a4ffcb31cef09c6c9412b9b9bd0b529f35c
FMarno <finlay.marno@gmail.com>
2019-12-28 21:43:12 +0000

    finished sixteen, had to look up solution for fast solve

--- sixteen/src/main.rs
+++ sixteen/src/main.rs
@@ -1,24 +1,50 @@
+use rayon::prelude::*;
 use std::fs;
-use std::iter;
 
 fn main() {
     let code = fs::read_to_string("sixteen/input").unwrap();
-    let mut seq: Vec<i32> = code
+    let seq: Vec<i32> = code
         .chars()
         .filter_map(|x| x.to_digit(10))
         .map(|x| x as i32)
         .collect();
-    for _ in 0..100 {
-        seq = phase(seq);
+    let skip = code[0..7].parse::<usize>().unwrap();
+    println!("{:?}", part1(seq.to_owned()));
+    println!("{:?}", part2(seq, skip));
+}
+
+fn part2(code: Vec<i32>, skip: usize) -> Vec<i32> {
+    let size = code.len() * 10000;
+    if skip < size / 2 {
+        panic!("this doesn't work on the first half");
     }
-    println!("{:?}", seq);
-    let out = seq[0..8].to_vec();
-    println!("{:?}", out);
+    let mut seq: Vec<_> = code.into_iter().cycle().take(size).skip(skip).collect();
+    seq.reverse();
+    for _ in 0..100 {
+        seq = seq
+            .into_iter()
+            .scan(0, |state, x| {
+                *state = (*state + x).abs() % 10;
+                Some(*state)
+            })
+            .collect();
+    }
+    let mut out = seq[seq.len() - 8..seq.len()].to_vec();
+    out.reverse();
+    out
+}
+
+fn part1(mut numbers: Vec<i32>) -> Vec<i32> {
+    for _ in 0..100 {
+        numbers = phase(numbers.to_owned());
+    }
+    numbers[0..8].to_vec()
 }
 
 fn phase(numbers: Vec<i32>) -> Vec<i32> {
     let n_len = numbers.len();
     (1..n_len + 1)
+        .into_par_iter()
         .map(|i| {
             let s: i32 = numbers[i - 1..]
                 .chunks(i)
@@ -34,14 +60,4 @@ fn phase(numbers: Vec<i32>) -> Vec<i32> {
             s.abs() % 10
         })
         .collect()
-}
-
-fn _gen_pattern(r: usize) -> impl Iterator<Item = i32> {
-    iter::repeat(0)
-        .take(r)
-        .chain(iter::repeat(1).take(r))
-        .chain(iter::repeat(0).take(r))
-        .chain(iter::repeat(-1).take(r))
-        .cycle()
-        .skip(1)
 }
